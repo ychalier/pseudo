6a0993a5b6fb9455400fce87d1119ea14c71f4b5
Yohan Chalier <yohan@chalier.fr>
2023-12-23 12:36:49 +0100

    added firstnames

--- www/master.js
+++ www/master.js
@@ -1,19 +1,48 @@
 window.addEventListener("load", () => {
 
-    const MINIMUM_OCCURRENCES = 1;
+    const MINIMUM_TOKEN_OCCURRENCES = 10;
 
     class Model {
-        constructor(tokens, maxs) {
+
+        constructor(tokens, maleFirstnames, femaleFirstnames) {
             this.tokens = tokens;
-            this.maxs = maxs;
+            this.tokenOccurrences = null;
+            this.maximumPairOccurrences = null;
+            this.maleFirstnames = maleFirstnames;
+            this.femaleFirstnames = femaleFirstnames;
+            this.preprocess();
+            console.log(
+                "Loaded model with",
+                Object.keys(this.tokens).length,
+                "tokens and",
+                this.maleFirstnames.size + this.femaleFirstnames.size,
+                "firstnames");
         }
+
+        preprocess() {
+            this.tokenOccurrences = {};
+            this.maximumPairOccurrences = {};
+            for (const token in this.tokens) {
+                const length = token.length;
+                if (!(length in this.maximumPairOccurrences)) {
+                    this.maximumPairOccurrences[length] = 0;
+                }
+                this.tokenOccurrences[token] = 0;
+                for (const letter in this.tokens[token]) {
+                    this.tokenOccurrences[token] += this.tokens[token][letter];
+                    this.maximumPairOccurrences[length] = Math.max(
+                        this.maximumPairOccurrences[length],
+                        this.tokens[token][letter]);
+                }
+            }
+        }
+
     }
 
     var model;
 
     function loadModelTsv(tsvText) {
         const tokens = {};
-        const maxs = {};
         let lines = tsvText.replaceAll("\r", "").split("\n");
         let header = lines[0].split("\t").slice(1);
         lines.slice(1).forEach(line => {
@@ -21,18 +50,23 @@ window.addEventListener("load", () => {
             let token = line.slice(0, line.indexOf("\t"));
             let length = token.length;
             tokens[token] = {};
-            if (!(length in maxs)) {
-                maxs[length] = 0;
-            }
             let values = line.slice(length + 1).split("\t");
             for (let i = 0; i < header.length; i++) {
                 if (values[i] == "") continue;
                 let value = parseInt(values[i]);
                 tokens[token][header[i]] = value;
-                maxs[length] = Math.max(maxs[length], values[i]);
             }
         });
-        model = new Model(tokens, maxs);
+        return tokens;
+    }
+
+    function loadFirstnamesText(firstnamesText) {
+        const firstnames = new Set();
+        firstnamesText.trim().replaceAll("\r", "").split("\n").forEach(line => {
+            if (line == "") return;
+            firstnames.add(line);
+        });
+        return firstnames;
     }
 
     function getZipEntry(zipEntries, filename) {
@@ -45,11 +79,20 @@ window.addEventListener("load", () => {
     
     function loadModelBlob(blob) {
         const zipFileReader = new zip.BlobReader(blob);
-        const modelTsvWriter = new zip.TextWriter();
+        const tokenTsvWriter = new zip.TextWriter();
+        const maleFirstnamesTextWriter = new zip.TextWriter();
+        const femaleFirstnamesTextWriter = new zip.TextWriter();
         const zipReader = new zip.ZipReader(zipFileReader);
         zipReader.getEntries().then(zipEntries => {
-            getZipEntry(zipEntries, "model.tsv").getData(modelTsvWriter).then(tsvText => {
-                loadModelTsv(tsvText);
+            getZipEntry(zipEntries, "tokens.tsv").getData(tokenTsvWriter).then(tsvText => {
+                const tokens = loadModelTsv(tsvText);
+                getZipEntry(zipEntries, "male.txt").getData(maleFirstnamesTextWriter).then(maleFirstnamesText => {
+                    const maleFirstnames = loadFirstnamesText(maleFirstnamesText);
+                    getZipEntry(zipEntries, "female.txt").getData(femaleFirstnamesTextWriter).then(femaleFirstnamesText => {
+                        const femaleFirstnames = loadFirstnamesText(femaleFirstnamesText);
+                        model = new Model(tokens, maleFirstnames, femaleFirstnames);
+                    });
+                });
             });
         });
     }
@@ -102,7 +145,7 @@ window.addEventListener("load", () => {
                 const length = token.length;
                 if (!(token in model.tokens)) continue;
                 if (!("$" in model.tokens[token])) continue;
-                score = model.tokens[token]["$"] / model.maxs[length] * Math.pow(10, length - 1);
+                score = model.tokens[token]["$"] / model.maximumPairOccurrences[length] * Math.pow(10, length - 1);
                 break;
             }
             const result = {
@@ -120,13 +163,13 @@ window.addEventListener("load", () => {
                 const token = suffix.value;
                 const length = token.length;
                 if (!(token in model.tokens)) continue;
+                if (model.tokenOccurrences[token] < MINIMUM_TOKEN_OCCURRENCES) continue;
                 let nextLetterCandidates = new Set(Object.keys(model.tokens[token]));
                 nextLetterCandidates = [...setsDifference(setsIntersection(nextLetterCandidates, lettersSet), seen)];
                 nextLetterCandidates.sort((a, b) => model.tokens[token][b] - model.tokens[token][a]);
                 for (const letter of nextLetterCandidates) {
-                    if (model.tokens[token][letter] <= MINIMUM_OCCURRENCES) continue;
                     seen.add(letter);
-                    const score = model.tokens[token][letter] / model.maxs[length] * Math.pow(10, length - 1);
+                    const score = model.tokens[token][letter] / model.maximumPairOccurrences[length] * Math.pow(10, length - 1);
                     const continuations = iteratePermutationsAux(model, word + letter, copyArrayWithout(letters, letter));
                     while (true) {
                         const continuation = continuations.next();
@@ -178,6 +221,61 @@ window.addEventListener("load", () => {
         }
     }
 
+    function* iterateFirstnames(model, query, male=true, female=true) {
+        let firstnameCandidates = null;
+        if (male && female) {
+            firstnameCandidates = new Set([...model.maleFirstnames, ...model.femaleFirstnames]);
+        } else if (male) {
+            firstnameCandidates = new Set([...model.maleFirstnames]);
+        } else if (female) {
+            firstnameCandidates = new Set([...model.femaleFirstnames]);
+        } else {
+            firstnameCandidates = new Set();
+        }
+        for (const firstname of firstnameCandidates) {
+            if (query.startsWith(firstname)) continue;
+            const pool = [...query];
+            let feasible = true;
+            for (const letter of firstname) {
+                const letterIndex = pool.indexOf(letter);
+                if (letterIndex == -1) {
+                    feasible = false;
+                    break;
+                }
+                pool.splice(letterIndex, 1);
+            }
+            if (!feasible) continue;
+            const result = {
+                firstname: firstname,
+                pool: pool,
+            }
+            yield result;
+        }
+    }
+
+    function findNicknames(model, query, options) {
+        const iterator = iterateFirstnames(model, query, options.male, options.female);
+        const results = [];
+        while (true) {
+            const item = iterator.next();
+            if (item.done) break;
+            const topPermutationsResult = computeTopPermutations(model, item.value.pool.join(""), options.k, options.timeout);
+            for (const permutation of topPermutationsResult.permutations) {
+                results.push({
+                    firstname: item.value.firstname,
+                    lastname: permutation.word,
+                    score: permutation.score,
+                });
+            }
+        }
+        results.sort((a, b) => b.score - a.score);
+        return results.slice(0, options.q);
+    }
+
+    function capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
     document.getElementById("form-query").addEventListener("submit", (event) => {
         event.preventDefault();
         if (model == null) alertAndThrowError("Model not loaded");
@@ -186,14 +284,23 @@ window.addEventListener("load", () => {
         const output = document.getElementById("pre-output");
         output.innerHTML = "Computing…";
         setTimeout(() => {
-            const result = computeTopPermutations(model, query);
-            output.innerHTML = "";
-            if (result.timeout) {
-                output.innerHTML += "Computation timed out. Results might be bad.\n";
-            }
-            result.permutations.forEach(permuation => {
-                output.innerHTML += `${permuation.word} (${permuation.score})\n`
+            const nicknames = findNicknames(model, query, {
+                male: true,
+                female: true,
+                k: 3,
+                q: 10,
+                timeout: 1
             });
+            output.innerHTML = "";
+            nicknames.forEach(nickname => {
+                output.innerHTML += `${capitalize(nickname.firstname)} ${capitalize(nickname.lastname)} (${nickname.score.toFixed(3)})\n`;
+            });
+            // if (result.timeout) {
+            //     output.innerHTML += "Computation timed out. Results might be bad.\n";
+            // }
+            // result.permutations.forEach(permuation => {
+            //     output.innerHTML += `${permuation.word} (${permuation.score})\n`
+            // });
         }, 1);
     });
     
