22c669b790017f8c41d3e79a641462048b0128e5
Yohan Chalier <yohan@chalier.fr>
2023-12-27 11:23:36 +0100

    cleaned training script

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import io
 import os
 import re
 import unicodedata
+from typing import Self
 
 
 class Trainer:
@@ -22,24 +23,39 @@ class Trainer:
         s = Trainer.IGNORED_CHARS.sub(" ", s)
         s = re.sub(r" +", " ", s)
         return s
+    
+    @staticmethod
+    def expand_input_paths(*paths: str) -> list[str]:
+        output = []
+        for path in paths:
+            if os.path.isfile(path):
+                output.append(path)
+            elif os.path.isdir(path):
+                for filename in next(os.walk(path))[2]:
+                    output.append(os.path.join(path, filename))
+            else:
+                for fpath in glob.glob(path):
+                    if os.path.isfile(fpath):
+                        output.append(fpath)
+        return output
 
-    def __init__(self, max_token_length:int=4) -> None:
-        self.max_token_length: int = max_token_length
-        self.tokens: dict[str, dict[str, int]] = {}
+    def __init__(self, max_token_length: int=4) -> None:
+        self._max_token_length: int = max_token_length
+        self._tokens: dict[str, dict[str, int]] = {}
 
-    def feed_word(self, word: str) -> None:
+    def _feed_word(self, word: str) -> None:
         word = f"^{word}$"
         for i in range(len(word) - 1):
-            for j in range(self.max_token_length):
+            for j in range(self._max_token_length):
                 if i + j + 1>= len(word):
                     continue
                 token = word[i:i+j+1]
                 char = word[i + j + 1]
-                self.tokens.setdefault(token, {})
-                self.tokens[token].setdefault(char, 0)
-                self.tokens[token][char] += 1
+                self._tokens.setdefault(token, {})
+                self._tokens[token].setdefault(char, 0)
+                self._tokens[token][char] += 1
 
-    def feed(self, fs:io.TextIOWrapper, buffer_size:int=4096) -> None:        
+    def _feed(self, fs: io.TextIOWrapper, buffer_size: int=4096) -> None:        
         prefix = ""
         while True:
             buffer = fs.read(buffer_size)
@@ -48,51 +64,47 @@ class Trainer:
             split = (prefix + self.clean_string(buffer)).split(" ")
             prefix = split[-1]
             for word in filter(lambda s: s, split[:-1]):
-                self.feed_word(word)
+                self._feed_word(word)
 
-    def train(self, *paths:str) -> None:
-        for path in paths:
+    def train(self, *paths: str) -> Self:
+        for path in self.expand_input_paths(*paths):
             print("Feeding text at", path)
             with open(path, "r", encoding="utf8") as file:
-                self.feed(file)
-        print("Model contains", len(self.tokens), "tokens")
+                self._feed(file)
+        print("Model contains", len(self._tokens), "tokens")
+        return self
     
-    def to_tsv(self, path:str="tokens.tsv") -> None:
+    def to_tsv(self, path: str) -> Self:
         print("Saving to", path)
         with open(path, "w") as file:
-            file.write("\t" + "\t".join(list(Trainer.ALLOWED_CHARS + "$")) + "\n")
-            for token in self.tokens:
+            file.write("\t" + "\t".join(Trainer.ALLOWED_CHARS + "$") + "\n")
+            for token in self._tokens:
                 file.write(f"{token}")
                 for c in Trainer.ALLOWED_CHARS + "$":
-                    file.write(f"\t{self.tokens[token].get(c, '')}")
+                    file.write(f"\t{self._tokens[token].get(c, '')}")
                 file.write("\n")
-
-
-def expand_input_paths(*paths):
-    output = []
-    for path in paths:
-        if os.path.isfile(path):
-            output.append(path)
-        elif os.path.isdir(path):
-            for filename in next(os.walk(path))[2]:
-                output.append(os.path.join(path, filename))
-        else:
-            for fpath in glob.glob(path):
-                if os.path.isfile(fpath):
-                    output.append(fpath)
-    return output
+        return self
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("paths", nargs="+", type=str)
-    parser.add_argument("-o", "--output-path", type=str, default="data/tokens.tsv", help="path to output TSV")
-    parser.add_argument("-m", "--max-token-length", type=int, default=4, help="maximum token length")
+    parser.add_argument(
+        "paths",
+        type=str,
+        nargs="+",
+        help="paths to input text files")
+    parser.add_argument(
+        "-o", "--output-path",
+        type=str,
+        default="data/tokens.tsv",
+        help="path to output TSV")
+    parser.add_argument(
+        "-m", "--max-token-length",
+        type=int,
+        default=4,
+        help="maximum token length")
     args = parser.parse_args()
-    trainer = Trainer(args.max_token_length)
-    input_paths = expand_input_paths(*args.paths)
-    trainer.train(*input_paths)
-    trainer.to_tsv(args.output_path)
+    Trainer(args.max_token_length).train(*args.paths).to_tsv(args.output_path)
 
 
 if __name__ == "__main__":
