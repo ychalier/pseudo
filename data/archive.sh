c2e443459ac17c4efe38c0813493a407b9ac479d
Yohan Chalier <yohan@chalier.fr>
2023-12-28 15:05:30 +0100

    added elves prefix list

--- data/archive.sh
+++ data/archive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 parent_path=$(dirname "${BASH_SOURCE[0]}")
-zip "$parent_path/../www/model.zip" "$parent_path/tokens.tsv" "$parent_path/firstnames.txt" "$parent_path/streets.txt"
+zip "$parent_path/../www/model.zip" "$parent_path/tokens.tsv" "$parent_path/firstnames.txt" "$parent_path/streets.txt" "$parent_path/elves.txt"
