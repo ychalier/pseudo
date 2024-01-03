970ad79592ca0692cfc33b4636018a9df4795110
Yohan Chalier <yohan@chalier.fr>
2024-01-03 17:17:09 +0100

    added fantasy prefixes

--- data/archive.ps1
+++ data/archive.ps1
@@ -1,2 +1,2 @@
 Set-Location $PSScriptRoot
-zip $PSScriptRoot\..\www\model.zip tokens.tsv firstnames.txt streets.txt
+zip $PSScriptRoot\..\www\model.zip tokens.tsv fantasy.txt firstnames.txt streets.txt
--- data/archive.sh
+++ data/archive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 parent_path=$(dirname "${BASH_SOURCE[0]}")
-zip "$parent_path/../www/model.zip" "$parent_path/tokens.tsv" "$parent_path/firstnames.txt" "$parent_path/streets.txt"
+zip "$parent_path/../www/model.zip" "$parent_path/tokens.tsv" "$parent_path/fantasy.txt" "$parent_path/firstnames.txt" "$parent_path/streets.txt"
